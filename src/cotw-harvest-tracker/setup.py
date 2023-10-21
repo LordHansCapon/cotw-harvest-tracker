dd806be8be1cca09cd581c20ae8c24a0c4dd6b73
LordHansCapon <zt.kohlberger@gmail.com>
2023-10-21 11:53:35 +0200

    Fixed harvest tracker setup.py

--- src/cotw-harvest-tracker/setup.py
+++ src/cotw-harvest-tracker/setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
 
 setup(
-   name='CotW Harvest Trackr',
+   name='CotW Harvest Tracker',
    version='1.9',
    author='LordHansCapon',
    install_requires=['pymem'],
+   py_modules=[]
 )
